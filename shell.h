703843556577af2773460acb57403bf2b88085ac
“mavine4512” <mavineoyugi@gmail.com>
2023-05-11 06:27:38 -0700

    added _atoi intergers

--- shell.h
+++ shell.h
@@ -31,8 +31,8 @@
 #define USE_GETLINE 0
 #define USE_STRTOK 0
 
-#define HIST_FILE       ".somple_shell_history"
-#define HIST_MAX        4096
+#define HIST_FILE	".somple_shell_history"
+#define HIST_MAX	4096
 
 exter char **environ;
 
@@ -118,7 +118,7 @@ int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
 
-/* toem_parser.c */
+/* parser.c */
 int is_cmd(info_t *, char *);
 char *dup_chars(char *, int, int);
 char *find_path(info *, char *, char *);
@@ -126,75 +126,75 @@ char *find_path(info *, char *, char *);
 /*loopsh.c*/
 int loophsh(char **);
 
-/* toem_errors.c */
+/* errors.c */
 void _eputs(char *);
 int _eputchar(char);
 int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
-/* toem_string.c */
+/* string.c */
 int _strlen(char *);
 int _strcmp(char *, char *);
-char *starts_with(const char *, const char *)
-	char *_strcat(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
 
-	/* toem_string1.c */
-	char *_strcpy(char *, char *);
-	char *_strdup(const char *);
-	void _puts(char *);
-	int _putchar(char);
+/* string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
 
-	/* toem_exits.c */
-	char *_strncpy(char *, char *, int);
-	char *_strncat(char *, char *, int);
-	char *_strchr(char *, char);
+/* exits.c */
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
 
-	/* toem _tokenizer.c */
-	char **strtow(char *, char *);
-	char **strtwo2(char *, char);
+/* tokenizer.c */
+char **strtow(char *, char *);
+char **strtwo2(char *, char);
 
-	/* toem_realloc.c */
-	char *_memset(char *, char, unsigned int);
-	void ffree(char **);
-	void *_realloc(void *, unsigned int, unsigned int);
+/* realloc.c */
+char *_memset(char *, char, unsigned int);
+void ffree(char **);
+void *_realloc(void *, unsigned int, unsigned int);
 
-	/* toem_memory.c */
-	int bfree(void **);
+/* memory.c */
+int bfree(void **);
 
-	/* toem_atoi.c */
-	int interactive(info_t *);
-	int is_delim(char, char *);
-	int _isalpha(int);
-	int _atoi(char *);
+/* _atoi.c */
+int inter_active(info_t *);
+int _delim(char, char *);
+int _isalpha(int);
+int _atoi(char *);
 
-	/* toem_errors1.c */
-	int _erratoi(char *);
-	void print_error(info_t *, char *);
-	int print_d(int, int);
-	char *convert_number(long int, int, int);
-	void remove_comments(char *);
+/* errors1.c */
+int _erratoi(char *);
+void print_error(info_t *, char *);
+int print_d(int, int);
+char *convert_number(long int, int, int);
+void remove_comments(char *);
 
-	/* toem_builtin.c */
-	int _myexit(info_t *);
-	int _mycd(info_t *);
-	int _myhelp(info_t *);
+/* toem_builtin.c */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
 
-	/* toem_builtin1.c */
-	int _myhistory(info_t *);
-	int _myalias(info_t *);
+/* toem_builtin1.c */
+int _myhistory(info_t *);
+int _myalias(info_t *);
 
-	/* toem_getline.c */
-	ssize_t get_input(info_t *);
-	int _getline(infor *, char **, size_t *);
-	void sigintHandler(int);
+/* toem_getline.c */
+ssize_t get_input(info_t *);
+int _getline(infor *, char **, size_t *);
+void sigintHandler(int);
 
-	/* toem_getlinfo.c */
-	void clear_info(info_t *);
-	void set_info(info_t *, char **);
-	void free_info(info_t *, int);
+/* toem_getlinfo.c */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
 
-	/* toem_getenv.c*/
-	char **get_environ(info_t *);
+/* toem_getenv.c*/
+char **get_environ(info_t *);
 int _unsetenv(info_t *, char *)
 	int _setenv(info_t *, char *, char *);
 
