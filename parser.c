cc885ab3aa812cfdb3c7594ed541cefeb720ad02
“mavine4512” <mavineoyugi@gmail.com>
2023-05-25 14:38:52 -0700

    fixed more bugs

--- parser.c
+++ parser.c
@@ -32,18 +32,16 @@ int is_cmd(info_t *info, char *path)
 char *dup_chars(char *pathstr, int start, int stop)
 {
 	static char buf[1024];
-	char *ptr = buf;
-	int i;
+	int k = 0, i = 0;
 
-	for (i = start; i < stop; i++)
+	for (k = 0, i = start; i < stop; i++)
 	{
 		if (pathstr[i] != ':')
 		{
-			*ptr = pathstr[i];
-			ptr++;
+			buf[k++] = pathstr[i];
 		}
 	}
-	*ptr = '\0';
+	buf[k] = 0;
 	return (buf);
 }
 
