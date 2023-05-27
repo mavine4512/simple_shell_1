9fbb3383c795769dea25ca983cd8f2086bb1b16e
“mavine4512” <mavineoyugi@gmail.com>
2023-05-27 08:13:54 -0700

    fixed vars

--- vars.c
+++ vars.c
@@ -23,6 +23,11 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		j++;
 		info->cmd_buf_type = CMD_AND;
 	}
+	else if (buf[j] == ';')
+	{
+		buf[j] = 0;
+		info->cmd_buf_type = CMD_CHAIN;
+	}
 	else
 		return (0);
 
